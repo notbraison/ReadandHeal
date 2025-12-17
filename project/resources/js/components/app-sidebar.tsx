202ffb78f550df80f7583459443a70f5142d3e42
notbraison <orinabraison@gmail.com>
2025-12-17 13:13:02 +0300

    linked homepage and dashboard

--- project/resources/js/components/app-sidebar.tsx
+++ project/resources/js/components/app-sidebar.tsx
@@ -10,7 +10,7 @@ import {
     SidebarMenuButton,
     SidebarMenuItem,
 } from '@/components/ui/sidebar';
-import { dashboard } from '@/routes';
+import { dashboard, home } from '@/routes';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
 import { Folder, House, LayoutGrid } from 'lucide-react';
@@ -19,7 +19,7 @@ import AppLogo from './app-logo';
 const mainNavItems: NavItem[] = [
     {
         title: 'Home',
-        href: '',
+        href: home(),
         icon: House,
     },
     {
